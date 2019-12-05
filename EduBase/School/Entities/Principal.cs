21e0b7aa15fdfa086b260dc81d0d1dbb5139b57d
rshanthos89 <55778564+rshanthos89@users.noreply.github.com>
2019-12-05 12:22:51 +0530

    Create Principal.cs

---
+++ EduBase/School/Entities/Principal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace School.Entities
+{
+    class Principal
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+    }
+}
