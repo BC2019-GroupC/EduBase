4de8fb8c4b3a90a267b5bb9511fb0635afc8405b
rshanthos89 <55778564+rshanthos89@users.noreply.github.com>
2019-12-05 12:22:46 +0530

    Create Admin.cs

---
+++ EduBase/School/Entities/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace School.Entities
+{
+    public class Admin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        
+
+
+    }
+}
