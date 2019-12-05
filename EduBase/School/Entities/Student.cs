baf6af7341f9d851e136813f4dca46fef1bd92b9
rshanthos89 <55778564+rshanthos89@users.noreply.github.com>
2019-12-05 12:22:57 +0530

    Create Student.cs

---
+++ EduBase/School/Entities/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace School.Entities
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Grade { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Section { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Gender { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public int ContactNumber { get; set; }
+    }
+}
